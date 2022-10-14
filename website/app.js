0e68213a3d9c7551dae980e624961d616b2963d4
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2022-10-14 11:22:28 +0200

    fix date bug

--- website/app.js
+++ website/app.js
@@ -4,7 +4,7 @@ const apiKey = '&appid=f7fb24f7a25261a7d21e7f76c870787c';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 // gets Weather data when generate button clicked
 document.getElementById('generate').addEventListener('click', performAction);
